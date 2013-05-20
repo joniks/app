f7813c49ae065919e0e74b79340fb58ecc5fe23b
Tobias Munk <schmunk@usrbin.de>
2013-05-20 19:33:52 +0200

    debug

---
+++ app/components/EditableField.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tobias
+ * Date: 20.05.13
+ * Time: 17:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+class EditableField extends CWidget {
+
+}
